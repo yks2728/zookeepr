4138dd9a806fe55fa919b32727f3f81063f58d8c
Zack Greenfield <zackg007@verizon.net>
2022-02-13 21:28:13 -0500

    added commented out app.delete function for potential re-submission

--- server.js
+++ server.js
@@ -19,3 +19,26 @@ app.use('/', htmlRoutes);
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+// DELETE request for notes
+// app.delete("/api/notes/:id", (req, res) => {
+//     const newNotes = [];
+//     console.log("Delete Path Triggered");
+//     for (let i = 0; i < notes.length; i++) {
+//       const note = notes[i]; 
+//       console.log("note", note.id);
+//       if (note.id !== req.params.id) {
+//         newNotes.push(note);
+//         console.log(i);
+//       }
+//     }
+//     fs.writeFileSync("./db/db.json", JSON.stringify(newNotes), (error) => {
+//       if (error) {
+//         console.log('There is an error', error);
+//       } else {
+//         console.log("ID was deleted");
+//       }
+//     })
+//     console.log(newNotes);
+//     res.json(newNotes);
+//   });
